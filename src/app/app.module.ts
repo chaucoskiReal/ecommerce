22a48b60564f03188a5f9aceeeb17e88c8e6ddc7
GABRIEL CANDINHO CHAUCOSKI <gabriel.chaucoski@alunos.sc.senac.br>
2023-08-22 19:43:37 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { CategoriaComponent } from './categoria/categoria.component';
+import { CategoriaListarComponent } from './categoria/categoria-listar/categoria-listar.component';
+import { CategoriaFormComponent } from './categoria/categoria-form/categoria-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MenuComponent,
+    HeaderComponent,
+    HomeComponent,
+    CategoriaComponent,
+    CategoriaListarComponent,
+    CategoriaFormComponent
   ],
   imports: [
     BrowserModule,
